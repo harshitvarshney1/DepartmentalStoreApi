4be52d1306d0b87b70e756cde9bb732a9a951816
Harshit <harshit.varshney@taazaa.com>
2021-05-31 12:45:04 +0530

    DepartmentalStoreApi

---
+++ DepartmentalStoreApi/DepartmentalStoreApi/Controllers/StaffsController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using DepartmentalStoreApi.Entities;
+using DepartmentalStoreApi.Infrastructure;
+using DepartmentalStoreApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentalStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffsController : ControllerBase
+    {
+        private  DepartmentStoreContext _context;
+        private readonly IMapper _mapper;
+        public StaffsController(DepartmentStoreContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public StaffModel[] getStaff()
+        {
+            IQueryable<Staff> query = _context.Staff.Include(c => c.Address).Include(r => r.Role);
+            var result = query.ToArray();
+            return _mapper.Map<StaffModel[]>(result);
+        }
+
+        [HttpGet("{id}")]
+        public StaffModel GetStaffById(long id)
+        {
+            {
+                IQueryable<Staff> query = _context.Staff.Include(c => c.Address).Where(t => t.Id == id);
+                var result = query.FirstOrDefault();
+                return _mapper.Map<StaffModel>(result);
+            }
+
+        }
+
+        [HttpGet("staff-members/{id}")]
+        public StaffModel GetBycode(int id, bool isAddress = false, string name = "")
+        {
+            IQueryable<Staff> query = _context.Staff
+            //.Include(c => c.Address)
+            .Include(t => t.Role);
+            if (isAddress)
+            {
+                query = query.Include(r => r.Address);
+            }
+            query.Where(x => x.Id == id);
+            var result = query.FirstOrDefault();
+            return _mapper.Map<StaffModel>(result);
+        }
+
+
+        [HttpPost]
+        public Staff createstaff(StaffPostModel staffitem)
+        {
+            _context.Staff.Add(_mapper.Map<Staff>(staffitem));
+            _context.SaveChanges();
+            return null;
+        }
+
+
+        [HttpPut("{id}")]
+        public Staff updateStaff(StaffPostModel staffitem, int id)
+        {
+            var query = _context.Staff.Where(i => i.Id == id).FirstOrDefault();
+            _mapper.Map(staffitem, query);
+            _context.SaveChanges();
+            return null;
+        }
+
+        [HttpGet("search")]
+        public StaffModel[] getStaffByQuery(string name )
+        {
+            IQueryable<Staff> query = _context.Staff.Include(c => c.Address).Include(r => r.Role);
+            var result = query.ToArray();
+            return _mapper.Map<StaffModel[]>(result);
+        }
+
+
+
+    }
+}
---
+++ DepartmentalStoreApi/DepartmentalStoreApi/Migrations/20210526113401_initial.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DepartmentalStoreApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AddressLine1 = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    AddressLine2 = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    City = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    State = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Pincode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Manufacturer = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ShortCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    CostPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    SellingPrice = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoleName = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productcategory",
+                columns: table => new
+                {
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productcategory", x => new { x.ProductId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_Productcategory_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Productcategory_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Gender = table.Column<char>(type: "character(1)", maxLength: 1, nullable: false),
+                    Salary = table.Column<decimal>(type: "numeric", nullable: false),
+                    AddressId = table.Column<long>(type: "bigint", nullable: false),
+                    RoleId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Staff_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Staff_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productcategory_CategoryId",
+                table: "Productcategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_AddressId",
+                table: "Staff",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Staff_RoleId",
+                table: "Staff",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productcategory");
+
+            migrationBuilder.DropTable(
+                name: "Staff");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
